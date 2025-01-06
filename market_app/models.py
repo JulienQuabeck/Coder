50c560aa543cf75322d870b47060304e3565aa51
unknown <j.quabeck@gmx.net>
2025-01-06 21:26:44 +0100

    added api folders

--- market_app/models.py
+++ market_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Offer(models.model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    image = ""
+    description = models.TextField(max_length=3000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    details = ""
+    min_price = models.DecimalField(max_digits=10, decimal_places=2)
+    min_delivery_time = models.IntegerField() 
+    user_details = ""
--- user_auth_app/models.py
+++ user_auth_app/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class UserProfile(models.Model):
+    types_Choices = [
+        ('business', 'Business'),
+        ('customer', 'Customer'),
+    ]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    file = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
+    location = models.CharField(max_length=255)
+    tel = models.CharField(max_length=20) 
+    description = models.TextField()
+    working_hours = models.TextField()
+    type = models.CharField(choices=types_Choices, default='customer')
+    email = models.EmailField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.type}"
