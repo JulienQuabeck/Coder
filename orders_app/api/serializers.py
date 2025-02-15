d078e9ba4ecf229b28dc3c9376991c25520b2066
unknown <j.quabeck@gmx.net>
2025-02-15 20:57:37 +0100

    problem with .00

--- orders_app/api/serializers.py
+++ orders_app/api/serializers.py
@@ -1,16 +1,22 @@
 from rest_framework import serializers
 from orders_app.models import OfferDetail, Orders, OrderDetail
 from user_auth_app.models import UserProfile
-from rest_framework.response import Response
-from rest_framework import status
-from decimal import Decimal, ROUND_DOWN
+from decimal import Decimal
 
+# class TwoDecimals(serializers.RelatedField):
+#     def to_representation(self, value):
+#         if isinstance(value, Decimal):
+#             return float(value.quantize(Decimal('0.01')))
+#         return value
+    
 class OrderGetSerializer(serializers.ModelSerializer):
     customer_user = serializers.IntegerField(source='customer_user.user.id', read_only=True)
     title = serializers.CharField(source='offer_detail_id.title', read_only=True)
     revisions = serializers.IntegerField(source='offer_detail_id.revisions', read_only=True)
     delivery_time_in_days = serializers.IntegerField(source='offer_detail_id.delivery_time_in_days', read_only=True)
-    price = serializers.DecimalField(source='offer_detail_id.price', max_digits=10, decimal_places=2, read_only=True, coerce_to_string=False)
+    price = serializers.SerializerMethodField()
+    # price = TwoDecimals(source='offer_detail_id.price', read_only=True)
+    # price = serializers.DecimalField(source='offer_detail_id.price', max_digits=10, decimal_places=2, read_only=True, coerce_to_string=False)
     features = serializers.JSONField(source='offer_detail_id.features', read_only=True)
     offer_type = serializers.CharField(source='offer_detail_id.offer_type', read_only=True)
 
@@ -19,6 +25,23 @@ class OrderGetSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'customer_user', 'business_user','title','revisions','delivery_time_in_days','price','features','offer_type', 'status', 'created_at', 'updated_at'
         ]
+
+    # def to_representation(self, instance):
+    #     data = super().to_representation(instance)
+    #     if 'price' in data and isinstance(data['price'], Decimal):
+    #         data['price'] = data['price'].quantize(Decimal('0.01'))
+    #     return data
+
+    # def to_representation(self, instance):
+    #     data = super().to_representation(instance)
+    #     data['price'] = float(format(Decimal(data['price']), '.2f'))
+    #     return data
+    
+    def get_price(self, obj):
+        if obj.offer_detail_id and obj.offer_detail_id.price is not None:
+            return float(format(Decimal(obj.offer_detail_id.price), '.2f'))
+        return None
+    
 
 class OrderPostSerializer(serializers.ModelSerializer):
 
